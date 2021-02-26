a347644e94ac517a0d9f95a8309d40b24ddabb55
ValeriyaGrach96 <tuul268@gmail.com>
2021-02-26 15:06:54 +0300

    added cycles and conditions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt("Сколько фильмов Вы посмотрели за последний год?", "");
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,12 +10,32 @@ const personalMovieDB = {
   privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = +prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = +prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = +prompt("На сколько оцените его по 10й шкале?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+  if (a != null && b != null
+   && a != "" && b != ""
+   && b <= 10
+   && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("Выполнено");
+  } else {
+    console.log("Error");
+    i--;
+  }
+}
 
 console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30){
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
+
+
